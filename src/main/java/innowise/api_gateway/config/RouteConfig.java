c564343ebcc7cee223e0efda807f78ca8f544830
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-08-04 17:19:00 +0300

    add: basic routing

---
+++ src/main/java/innowise/api_gateway/config/RouteConfig.java
@@ -0,0 +1,36 @@
+package innowise.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+    @Value("${services.url.user_service}")
+    private String userServiceUrl;
+
+    @Value("${services.url.auth_service}")
+    private String authServiceUrl;
+
+    @Value("${services.url.order_service}")
+    private String orderServiceUrl;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/user/**",
+                                "/api/card/**")
+                        .uri(userServiceUrl))
+                .route(p -> p
+                        .path("/api/auth/**")
+                        .uri(authServiceUrl))
+                .route(p -> p
+                        .path("/api/order/**",
+                                "/api/item/**")
+                        .uri(orderServiceUrl))
+                .build();
+    }
+}
